655f7ccf4386490998834105011aa1c325f487c2
nguyenhoangkhoi06011 <nguyenhoangkhoi012021@gmail.com>
2025-02-24 15:35:05 +0700

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace Bai14_TinhToanTrongDaySo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtSo1 = new System.Windows.Forms.TextBox();
+            this.txtSo2 = new System.Windows.Forms.TextBox();
+            this.btnTinh = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtTC = new System.Windows.Forms.TextBox();
+            this.txtle = new System.Windows.Forms.TextBox();
+            this.txtTong = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtSo1
+            // 
+            this.txtSo1.Location = new System.Drawing.Point(59, 48);
+            this.txtSo1.Name = "txtSo1";
+            this.txtSo1.Size = new System.Drawing.Size(90, 20);
+            this.txtSo1.TabIndex = 0;
+            // 
+            // txtSo2
+            // 
+            this.txtSo2.Location = new System.Drawing.Point(59, 103);
+            this.txtSo2.Name = "txtSo2";
+            this.txtSo2.Size = new System.Drawing.Size(90, 20);
+            this.txtSo2.TabIndex = 0;
+            // 
+            // btnTinh
+            // 
+            this.btnTinh.Location = new System.Drawing.Point(38, 184);
+            this.btnTinh.Name = "btnTinh";
+            this.btnTinh.Size = new System.Drawing.Size(111, 35);
+            this.btnTinh.TabIndex = 1;
+            this.btnTinh.Text = "Tính";
+            this.btnTinh.UseVisualStyleBackColor = true;
+            this.btnTinh.Click += new System.EventHandler(this.btnTinh_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(268, 55);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Tổng chẳn";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(268, 110);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(15, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "lẻ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(268, 184);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(32, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Tổng";
+            // 
+            // txtTC
+            // 
+            this.txtTC.Location = new System.Drawing.Point(349, 52);
+            this.txtTC.Name = "txtTC";
+            this.txtTC.Size = new System.Drawing.Size(90, 20);
+            this.txtTC.TabIndex = 0;
+            // 
+            // txtle
+            // 
+            this.txtle.Location = new System.Drawing.Point(349, 107);
+            this.txtle.Name = "txtle";
+            this.txtle.Size = new System.Drawing.Size(90, 20);
+            this.txtle.TabIndex = 0;
+            // 
+            // txtTong
+            // 
+            this.txtTong.Location = new System.Drawing.Point(349, 177);
+            this.txtTong.Name = "txtTong";
+            this.txtTong.Size = new System.Drawing.Size(90, 20);
+            this.txtTong.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnTinh);
+            this.Controls.Add(this.txtSo2);
+            this.Controls.Add(this.txtTong);
+            this.Controls.Add(this.txtle);
+            this.Controls.Add(this.txtTC);
+            this.Controls.Add(this.txtSo1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtSo1;
+        private System.Windows.Forms.TextBox txtSo2;
+        private System.Windows.Forms.Button btnTinh;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtTC;
+        private System.Windows.Forms.TextBox txtle;
+        private System.Windows.Forms.TextBox txtTong;
+    }
+}
+
